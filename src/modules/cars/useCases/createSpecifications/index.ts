5dafdf36524bb23982bb686c2a861f25f38b5615
Douglas <dodo_nadoti@hotmail.com>
2022-03-12 19:15:21 -0300

    Primeiro commit

---
+++ src/modules/cars/useCases/createSpecifications/index.ts
@@ -0,0 +1,12 @@
+import { SpecificationsRepository } from "../../repositories/implementations/SpecificationsRepository";
+import { CreateSpecificationsController } from "./CreateSpecificationsController";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
+
+
+const specificationsRepository = new SpecificationsRepository()
+
+const createSpecificationUseCase = new CreateSpecificationUseCase(specificationsRepository)
+
+const createSpecificationController = new CreateSpecificationsController(createSpecificationUseCase)
+
+export { createSpecificationController }
